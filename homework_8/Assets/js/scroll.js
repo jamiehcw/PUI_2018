66713558ed6f97479c6ea49d9c9d2b8be8b6698d
jamiehcw <jamieh@andrew.cmu.edu>
2018-11-30 23:40:38 -0500

    comment update2

--- homework_8/Assets/js/scroll.js
+++ homework_8/Assets/js/scroll.js
@@ -1,11 +1,11 @@
 /* to allow for horizontal scroll while scrolling vertically on the track pad or the mouse */
 (function() {
-    function scrollHorizontally(e) {
-        e = window.event || e;
-        var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
-        document.documentElement.scrollLeft -= (delta*40); // Multiplied by 40
-        document.body.scrollLeft -= (delta*40); // Multiplied by 40
-        e.preventDefault();
+    function scrollHorizontally(h) {
+        h = window.event || h;
+        var delta = Math.max(-1, Math.min(1, (h.wheelDelta || -h.detail)));
+        document.documentElement.scrollLeft -= (delta*100); // Multiplied by 100 to change amount scrolled
+        document.body.scrollLeft -= (delta*100); // Multiplied by 100 to change amount scrolled
+        h.preventDefault();
     }
     if (window.addEventListener) {
         // Chrome
