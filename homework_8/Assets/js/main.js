33668cecafc7d305c3add83be5db12ba0cb7bb3f
jamiehcw <jamieh@andrew.cmu.edu>
2018-11-30 21:14:02 -0500

    rearranged for assets folder

---
+++ homework_8/Assets/js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.toggle').click(function(){
+        $('.toggle').toggleClass('active')
+        $('.overlay').toggleClass('active')
+        $('.overlay-content').toggleClass('active')
+    })
+    
+})
+
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "225px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
